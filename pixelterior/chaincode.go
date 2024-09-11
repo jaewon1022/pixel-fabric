1f6a1d7a95a9b6830e8f9773b21cdd3cb94bc9b8
jaewon1022 <andyjaewon@naver.com>
2024-09-11 13:40:46 +0900

    [pixelterior-go.v0.0.2] - 중복된 기능을 하는 체인 코드 제거   transferToken, trade 함수가 동일한 작업을 하는 체인 코드였기에, transferToken 체인 코드가 제거되었습니다.
    - 타입 구조 수정
     Wallet 타입에서 현금을 뜻하는 Balance 타입이 제거되었습니다.
     Token 타입에서 불필요한 Decimals 타입이 제거되었습니다.
     Asset 타입과 Token 타입은 각각 토큰을 의미하고있는 타입이였기에, Asset 타입이 제거되었습니다.
    
    - 불필요한 체인 코드 제거
      불필요한 체인 코드인 updateAsset이 제거되었습니다.
      토큰을 제거하는 기능인 deleteAsset 함수가 제거되었습니다.
    
    - 명칭 변경
      기존에 토큰을 뜻하는 단어인 asset이 token으로 수정되었습니다.

--- pixelterior/chaincode.go
+++ pixelterior/chaincode.go
@@ -13,53 +13,47 @@ type SimpleChaincode struct {
 }
 
 type Wallet struct {
-	Balance int            `json:"balance"`
 	Address string         `json:"address"`
 	Tokens  map[string]int `json:"tokens"`
 }
 
 type User struct {
-	Name    string `json:"name"`
-	Wallet  Wallet `json:"wallet"`
+	Name   string `json:"name"`
+	Wallet Wallet `json:"wallet"`
 }
 
 type Token struct {
-	Name     string `json:"name"`
-	Symbol   string `json:"symbol"`
-	Decimals int    `json:"decimals"`
-	TotalSupply int `json:"totalSupply"`
+	Name        string `json:"name"`
+	Symbol      string `json:"symbol"`
+	TotalSupply int    `json:"totalSupply"`
 }
 
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	fmt.Println("ex02 Init")
 
-    token := Token{
-        Name: "MyToken",
-        Symbol: "MTK",
-        Decimals: 18,
-        TotalSupply: 1000000,
-    }
-    tokenJSON, _ := json.Marshal(token)
+	token := Token{
+		Name:        "MyToken",
+		Symbol:      "MTK",
+		TotalSupply: 1000000,
+	}
+	tokenJSON, _ := json.Marshal(token)
 
-    stub.PutState("MTK", tokenJSON)
+	stub.PutState("MTK", tokenJSON)
 
 	user1 := User{
 		Name: "user1",
 		Wallet: Wallet{
 			Address: "0x1234",
-			Balance: 10000,
-			Tokens: map[string]int{"MTK": 500000},
+			Tokens:  map[string]int{"MTK": 500000},
 		},
 	}
 	user2 := User{
 		Name: "user2",
 		Wallet: Wallet{
 			Address: "0x5678",
-			Balance: 10000,
-			Tokens: map[string]int{"MTK": 500000},
+			Tokens:  map[string]int{"MTK": 500000},
 		},
 	}
-
 
 	user1JSON, _ := json.Marshal(user1)
 	user2JSON, _ := json.Marshal(user2)
@@ -77,10 +71,8 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	function, args := stub.GetFunctionAndParameters()
 
 	switch function {
-	case "transferToken":
-		return t.transferToken(stub, args)
-	case "createAsset":
-		return t.createAsset(stub, args)
+	case "mint":
+		return t.mint(stub, args)
 	case "createUser":
 		return t.createUser(stub, args)
 	case "deleteUser":
@@ -89,189 +81,152 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 		return t.deleteAllUsers(stub)
 	case "trade":
 		return t.trade(stub, args)
-	case "updateAsset":
-		return t.updateAsset(stub, args)
-	case "deleteAsset":
-		return t.deleteAsset(stub, args)
-	case "deleteAllAssets":
-		return t.deleteAllAssets(stub)
-	case "queryAssets":
-		return t.queryAssets(stub)
-	case "queryAsset":
-		return t.queryAsset(stub, args)
+	case "deleteAllTokens":
+		return t.deleteAllTokens(stub)
+	case "queryTokens":
+		return t.queryTokens(stub)
+	case "queryToken":
+		return t.queryToken(stub, args)
 	case "queryUsers":
 		return t.queryUsers(stub)
 	case "queryUser":
 		return t.queryUser(stub, args)
 	default:
-		return shim.Error("Invalid invoke function name")
+		return shim.Error("Invalid function name invoked")
 	}
 }
 
-func (t *SimpleChaincode) transferToken(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-    if len(args) != 3 {
-        return shim.Error("Incorrect number of arguments. Expecting 3: from, to, amount")
-    }
+func (t *SimpleChaincode) trade(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 4 {
+		return shim.Error("Incorrect number of arguments. Expecting 3: from, to, amount")
+	}
 
-    from := args[0]
-    to := args[1]
-    amount, err := strconv.Atoi(args[2])
-    if err != nil {
-        return shim.Error("Invalid amount: " + err.Error())
-    }
+	from := args[0]
+	to := args[1]
+	tokenSymbol := args[2]
+	amount, err := strconv.Atoi(args[3])
+	if err != nil {
+		return shim.Error("Invalid amount: " + err.Error())
+	}
 
-    // 송신자와 수신자의 상태 가져오기
-    fromBytes, err := stub.GetState(from)
-    if err != nil {
-        return shim.Error("Failed to get sender: " + err.Error())
-    }
-    if fromBytes == nil {
-        return shim.Error("Sender not found")
-    }
+	// 송신자와 수신자의 상태 가져오기
+	fromBytes, err := stub.GetState(from)
+	if err != nil {
+		return shim.Error("Failed to get sender: " + err.Error())
+	}
+	if fromBytes == nil {
+		return shim.Error("Sender not found")
+	}
 
-    toBytes, err := stub.GetState(to)
-    if err != nil {
-        return shim.Error("Failed to get recipient: " + err.Error())
-    }
-    if toBytes == nil {
-        return shim.Error("Recipient not found")
-    }
+	toBytes, err := stub.GetState(to)
+	if err != nil {
+		return shim.Error("Failed to get recipient: " + err.Error())
+	}
+	if toBytes == nil {
+		return shim.Error("Recipient not found")
+	}
 
-    var fromUser, toUser User
-    json.Unmarshal(fromBytes, &fromUser)
-    json.Unmarshal(toBytes, &toUser)
+	var fromUser, toUser User
+	json.Unmarshal(fromBytes, &fromUser)
+	json.Unmarshal(toBytes, &toUser)
 
-    // 잔액 확인
-    if fromUser.Tokens["MTK"] < amount {
-        return shim.Error("Insufficient balance")
-    }
+	// 잔액 확인
+	if fromUser.Wallet.Tokens[tokenSymbol] < amount {
+		return shim.Error("Insufficient balance")
+	}
 
-    // 토큰 전송
-    fromUser.Tokens["MTK"] -= amount
-    if toUser.Tokens == nil {
-        toUser.Tokens = make(map[string]int)
-    }
-    toUser.Tokens["MTK"] += amount
+	// 토큰 전송
+	if toUser.Wallet.Tokens == nil {
+		toUser.Wallet.Tokens = make(map[string]int)
+	}
 
-    // 상태 업데이트
-    fromJSON, _ := json.Marshal(fromUser)
-    toJSON, _ := json.Marshal(toUser)
-    stub.PutState(from, fromJSON)
-    stub.PutState(to, toJSON)
+	fromUser.Wallet.Tokens[tokenSymbol] -= amount
+	toUser.Wallet.Tokens[tokenSymbol] += amount
 
-    return shim.Success([]byte("Token transfer successful"))
+	// 상태 업데이트
+	fromJSON, _ := json.Marshal(fromUser)
+	toJSON, _ := json.Marshal(toUser)
+	stub.PutState(from, fromJSON)
+	stub.PutState(to, toJSON)
+
+	return shim.Success([]byte("Token transfer successful"))
 }
 
-func (t *SimpleChaincode) createAsset(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 3 {
+func (t *SimpleChaincode) mint(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 4 {
 		return shim.Error("Incorrect number of arguments. Expecting 3")
 	}
 
 	name := args[0]
-	assetKey := "asset_" + name
+	symbol := args[1]
+	totalAmount := args[2]
+	to := args[3]
 
-	assetBytes, err := stub.GetState(assetKey)
+	tokenKey := "token_" + symbol
+
+	symbolBytes, err := stub.GetState(tokenKey)
 	if err != nil {
-		return shim.Error("Failed to get asset")
+		return shim.Error("Failed to get tokens")
 	}
 
-	if assetBytes != nil {
-		return shim.Error("Inputed name of asset already exist")
+	var token Token
+	var totalSupply int
+
+	// 이미 토큰이 존재할 경우 총 발행량을 더하고, 존재하지 않을 경우 토큰을 새로 발행함
+	if symbolBytes != nil {
+		json.Unmarshal(symbolBytes, &token)
+
+		totalSupply, err = strconv.Atoi(totalAmount)
+		if err != nil {
+			return shim.Error("Invalid totalAmount inputed. Expecting integer value")
+		}
+
+		token.TotalSupply += totalSupply
+	} else {
+		totalSupply, err := strconv.Atoi(totalAmount)
+		if err != nil {
+			return shim.Error("Invalid totalAmount inputed. Expecting integer value")
+		}
+
+		token = Token{
+			Name:        name,
+			Symbol:      symbol,
+			TotalSupply: totalSupply,
+		}
 	}
 
-	price, err := strconv.Atoi(args[1])
+	// to 에게 토큰을 발행
+	toBytes, err := stub.GetState(to)
 	if err != nil {
-		return shim.Error("Invalid price inputed. Expecting integer value")
+		return shim.Error("Failed to get recipient")
+	}
+	if toBytes == nil {
+		return shim.Error("Recipient not found")
 	}
 
-	totalAmount, err := strconv.Atoi(args[2])
+	var toUser User
+	json.Unmarshal(toBytes, &toUser)
+
+	if toUser.Wallet.Tokens == nil {
+		toUser.Wallet.Tokens = make(map[string]int)
+	}
+
+	toUser.Wallet.Tokens[symbol] += totalSupply
+
+	tokenJSON, err := json.Marshal(token)
 	if err != nil {
-		return shim.Error("Invalid totalAmount inputed. Expecting integer value")
+		return shim.Error("Failed to marshal Token")
 	}
 
-	asset := Asset{
-		Name: name,
-		Price: price,
-		TotalStock: totalAmount,
-	}
-
-	assetJSON, err := json.Marshal(asset)
+	err = stub.PutState(tokenKey, tokenJSON)
 	if err != nil {
-		return shim.Error("Failed to marshal asset")
+		return shim.Error("Failed to create Token")
 	}
 
-	err = stub.PutState(assetKey, assetJSON)
-	if err != nil {
-		return shim.Error("Failed to create asset")
-	}
-
-	return shim.Success([]byte("Asset created successfully"))
+	return shim.Success([]byte("Token minted successfully"))
 }
 
-func (t *SimpleChaincode) updateAsset(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 3 {
-		return shim.Error("Incorrect number of arguments. Expecting 3: name, price, totalAmount")
-	}
-
-	name := args[0]
-	assetKey := "asset_" + name
-	price, _ := strconv.Atoi(args[1])
-	totalAmount, _ := strconv.Atoi(args[2])
-
-	assetBytes, err := stub.GetState(assetKey)
-	if err != nil {
-		return shim.Error("Failed to get asset")
-	}
-
-	if assetBytes == nil {
-		return shim.Error("Asset not found")
-	}
-
-	asset := Asset{
-		Name: name,
-		Price: price,
-		TotalStock: totalAmount,
-	}
-
-	assetJSON, err := json.Marshal(asset)
-	if err != nil {
-		return shim.Error("Failed to marshal asset")
-	}
-
-	err = stub.PutState(assetKey, assetJSON)
-	if err != nil {
-		return shim.Error(fmt.Sprintf("Failed to put state: %v", err))
-	}
-
-	return shim.Success([]byte("Asset updated successfully"))
-}
-
-func (t *SimpleChaincode) deleteAsset(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 1 {
-		return shim.Error("Incorrect number of arguments. Expecting 1")
-	}
-
-	name := args[0]
-	assetKey := "asset_" + name
-
-	assetBytes, err := stub.GetState(assetKey)
-	if err != nil {
-		return shim.Error("Failed to get asset")
-	}
-
-	if assetBytes == nil {
-		return shim.Error("Asset not found")
-	}
-
-	err = stub.DelState(assetKey)
-	if err != nil {
-		return shim.Error("Failed to delete asset")
-	}
-
-	return shim.Success([]byte("Asset deleted successfully"))
-}
-
-func (t *SimpleChaincode) deleteAllAssets(stub shim.ChaincodeStubInterface) pb.Response {
+func (t *SimpleChaincode) deleteAllTokens(stub shim.ChaincodeStubInterface) pb.Response {
 	iterator, err := stub.GetStateByRange("asset_", "asset_~")
 
 	if err != nil {
@@ -305,45 +260,44 @@ func (t *SimpleChaincode) deleteAllUsers(stub shim.ChaincodeStubInterface) pb.Re
 	return shim.Success([]byte("All users deleted successfully"))
 }
 
-func (t *SimpleChaincode) queryAssets(stub shim.ChaincodeStubInterface) pb.Response {
-	iterator, err := stub.GetStateByRange("asset_", "asset_~")
+func (t *SimpleChaincode) queryTokens(stub shim.ChaincodeStubInterface) pb.Response {
+	iterator, err := stub.GetStateByRange("token_", "token_~")
 
 	if err != nil {
-		return shim.Error("Failed to get assets")
+		return shim.Error("Failed to get tokens")
 	}
 	defer iterator.Close()
 
-	var assets []Asset
+	var tokens []Token
 	for iterator.HasNext() {
-		assetData, _ := iterator.Next()
-		var asset Asset
-		json.Unmarshal(assetData.Value, &asset)
-		assets = append(assets, asset)
+		tokenData, _ := iterator.Next()
+		var token Token
+		json.Unmarshal(tokenData.Value, &token)
+		tokens = append(tokens, token)
 	}
 
-	assetsBytes, _ := json.Marshal(assets)
-
-	return shim.Success(assetsBytes)
+	tokensBytes, _ := json.Marshal(tokens)
+	return shim.Success(tokensBytes)
 }
 
-func (t *SimpleChaincode) queryAsset(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (t *SimpleChaincode) queryToken(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	if len(args) != 1 {
 		return shim.Error("Incorrect number of arguments. Expecting 1")
 	}
 
 	name := args[0]
-	assetKey := "asset_" + name
+	tokenKey := "token_" + name
 
-	assetBytes, err := stub.GetState(assetKey)
+	tokenBytes, err := stub.GetState(tokenKey)
 	if err != nil {
-		return shim.Error("Failed to get asset")
+		return shim.Error("Failed to get token")
 	}
 
-	if assetBytes == nil {
-		return shim.Error("Asset not found")
+	if tokenBytes == nil {
+		return shim.Error("Token not found")
 	}
 
-	return shim.Success(assetBytes)
+	return shim.Success(tokenBytes)
 }
 
 func (t *SimpleChaincode) createUser(stub shim.ChaincodeStubInterface, args []string) pb.Response {
@@ -360,9 +314,8 @@ func (t *SimpleChaincode) createUser(stub shim.ChaincodeStubInterface, args []st
 	}
 
 	newUser := User{
-		Name: name, 
-		Wallet: Wallet{Balance: 10000, Address: "0xasdf", Assets: []MyAsset{{Name: "asset1", Amount: 10}}}, 
-		Tokens: make(map[string]int),
+		Name:   name,
+		Wallet: Wallet{Address: "0x", Tokens: make(map[string]int)},
 	}
 
 	newUserBytes, _ := json.Marshal(newUser)
@@ -397,94 +350,6 @@ func (t *SimpleChaincode) deleteUser(stub shim.ChaincodeStubInterface, args []st
 	return shim.Success([]byte("User deleted successfully"))
 }
 
-func (t *SimpleChaincode) trade(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 4 {
-		return shim.Error("Incorrect number of arguments. Expecting 4")
-	}
-
-	buyerId := args[0]
-	sellerId := args[1]
-	assetName := args[2]
-	quantity, err := strconv.Atoi(args[3])
-
-	if err != nil {
-		return shim.Error("invalid quantity inputed. Expecting integer value")
-	}
-
-	buyerBytes, err := stub.GetState(buyerId)
-	if err != nil || buyerBytes == nil {
-		return shim.Error("Buyer Id not found")
-	}
-
-	sellerBytes, err := stub.GetState(sellerId)
-	if err != nil || sellerBytes == nil {
-		return shim.Error("Seller Id not found")
-	}
-
-	var buyer, seller User
-	json.Unmarshal(buyerBytes, &buyer)
-	json.Unmarshal(sellerBytes, &seller)
-
-	assetBytes, err := stub.GetState("assets")
-	var assets map[string]Asset
-	json.Unmarshal(assetBytes, &assets)
-	asset, exists := assets[assetName]
-	if !exists {
-		return shim.Error("Asset not found")
-	}
-
-	// Find seller's asset
-	sellerAssetIndex := -1
-	for i, a := range seller.Wallet.Assets {
-		if a.Name == assetName {
-			if a.Amount < quantity {
-				return shim.Error("Seller does not have enough stock")
-			}
-			sellerAssetIndex = i
-			break
-		}
-	}
-	if sellerAssetIndex == -1 {
-		return shim.Error("Seller does not have the asset")
-	}
-
-	totalPrice := asset.Price * quantity
-
-	if buyer.Wallet.Balance < totalPrice {
-		return shim.Error("Buyer does not have enough balance")
-	}
-
-	// Update balances
-	buyer.Wallet.Balance -= totalPrice
-	seller.Wallet.Balance += totalPrice
-
-	// Update buyer's assets
-	buyerAssetIndex := -1
-	for i, a := range buyer.Wallet.Assets {
-		if a.Name == assetName {
-			buyer.Wallet.Assets[i].Amount += quantity
-			buyerAssetIndex = i
-			break
-		}
-	}
-	if buyerAssetIndex == -1 {
-		buyer.Wallet.Assets = append(buyer.Wallet.Assets, MyAsset{Name: assetName, Amount: quantity})
-	}
-
-	// Update seller's assets
-	seller.Wallet.Assets[sellerAssetIndex].Amount -= quantity
-	if seller.Wallet.Assets[sellerAssetIndex].Amount == 0 {
-		seller.Wallet.Assets = append(seller.Wallet.Assets[:sellerAssetIndex], seller.Wallet.Assets[sellerAssetIndex+1:]...)
-	}
-
-	buyerBytes, _ = json.Marshal(buyer)
-	sellerBytes, _ = json.Marshal(seller)
-	stub.PutState(buyerId, buyerBytes)
-	stub.PutState(sellerId, sellerBytes)
-
-	return shim.Success([]byte("Trade completed"))
-}
-
 func (t *SimpleChaincode) queryUsers(stub shim.ChaincodeStubInterface) pb.Response {
 	iterator, err := stub.GetStateByRange("", "")
 
@@ -511,30 +376,18 @@ func (t *SimpleChaincode) queryUser(stub shim.ChaincodeStubInterface, args []str
 	}
 
 	userId := args[0]
-	walletBytes, err := stub.GetState(userId)
 
-	if err != nil || walletBytes == nil {
-		return shim.Error("Wallet not found")
-	}
+	userBytes, err := stub.GetState(userId)
 
-	return shim.Success(walletBytes)
-}
-
-func (t *SimpleChaincode) getBalance(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var name string
-
-	name = args[0]
-
-	userData, err := stub.GetState(name)
 	if err != nil {
-		return shim.Error("Failed to get Data about inputed user: " + name)
+		return shim.Error("Failed to get user")
 	}
 
-	if userData == nil {
-		return shim.Error("Not Existing user name. Input other name")
+	if userBytes == nil {
+		return shim.Error("User not found")
 	}
 
-	return shim.Success(userData)
+	return shim.Success(userBytes)
 }
 
 func (t *SimpleChaincode) invoke(stub shim.ChaincodeStubInterface, args []string) pb.Response {
@@ -614,4 +467,3 @@ func (t *SimpleChaincode) query(stub shim.ChaincodeStubInterface, args []string)
 	fmt.Printf("Query Response:%s\n", jsonResp)
 	return shim.Success(Avalbytes)
 }
-
