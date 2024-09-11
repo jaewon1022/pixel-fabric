66e35d9d6c3f8ce0be6c63f63a8d07cbde434c6d
jaewon1022 <andyjaewon@naver.com>
2024-09-11 11:10:07 +0900

    testcode 작성 중

--- pixelterior/chaincode_test.go
+++ pixelterior/chaincode_test.go
@@ -57,9 +57,18 @@ func TestPixelterior_Invoke(t *testing.T) {
 	// Init Asset1{Name: "asset1", Price: 100, TotalStock: 10000}, Asset2{Name: "asset2", Price: 200, TotalStock: 10000}
 	checkInit(t, stub)
 
+	// Invoke createAsset asset3 100 10000
+	checkInvoke(t, stub, [][]byte{[]byte("createAsset"), []byte("Asset3"), []byte("100"), []byte("10000")})
+
 	// Invoke updateAsset asset1 price 100 -> 200
 	checkInvoke(t, stub, [][]byte{[]byte("updateAsset"), []byte("Asset1"), []byte("200"), []byte("10000")})
 
 	// Invoke queryAsset asset1
 	checkInvoke(t, stub, [][]byte{[]byte("queryAsset"), []byte("Asset1")})
+
+	// Invoke createUser User{Name: "TestUser", Wallet: Wallet{Balance: 10000, Address: "0xasdf", Assets: []MyAsset{Name: "asset1", Amount: 10}}}, Tokens: make(map[string]int)}
+	checkInvoke(t, stub, [][]byte{[]byte("createUser"), []byte("TestUser")})
+
+	// Invoke trade User1 -> User2 / Asset1 50ea
+	checkInvoke(t, stub, [][]byte{[]byte("trade"), []byte("User1"), []byte("User2"), []byte("Asset1"), []byte("50")})
 }
