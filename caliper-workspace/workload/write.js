1a69bf1492bc19bfe96c125854bf54989403ec11
jaewon1022 <andyjaewon@naver.com>
2024-09-13 13:05:43 +0900

    mint 체인 코드를 이용한 write transaction 측정으로 수정

--- caliper-workspace/workload/write.js
+++ caliper-workspace/workload/write.js
@@ -7,74 +7,78 @@ class ReadWorkload extends WorkloadModuleBase {
     super();
   }
 
-  async initializeWorkloadModule(
-    workerIndex,
-    totalWorkers,
-    roundIndex,
-    roundArguments,
-    sutAdapter,
-    sutContext
-  ) {
-    await super.initializeWorkloadModule(
-      workerIndex,
-      totalWorkers,
-      roundIndex,
-      roundArguments,
-      sutAdapter,
-      sutContext
-    );
+  // async initializeWorkloadModule(
+  //   workerIndex,
+  //   totalWorkers,
+  //   roundIndex,
+  //   roundArguments,
+  //   sutAdapter,
+  //   sutContext
+  // ) {
+  //   await super.initializeWorkloadModule(
+  //     workerIndex,
+  //     totalWorkers,
+  //     roundIndex,
+  //     roundArguments,
+  //     sutAdapter,
+  //     sutContext
+  //   );
 
-    for(let i = 1; i <= this.roundArguments.assets; i++) {
-      const assetId = `${this.workerIndex}-${i}`;
-      console.log(
-        `Creating asset "${this.workerIndex}-${i}" by workerNode ${workerIndex}`
-      );
+  //   for (let i = 1; i <= this.roundArguments.assets; i++) {
+  //     const assetId = `${this.workerIndex}-${i}`;
+  //     const tokenSymbol = `TTN${assetId}`;
 
-      let txArgs = {
-        contractId: this.roundArguments.contractId,
-        contractFunction: "createAsset",
-        invokerIdentity: "client",
-        contractArguments: [assetId, "100", "10000"],
-        readOnly: false,
-      };
+  //     console.log(
+  //       `Creating asset "${this.workerIndex}-${i}" by workerNode ${workerIndex}`
+  //     );
 
-      await this.sutAdapter.sendRequests(txArgs);
-    }
-  }
+  //     let txArgs = {
+  //       contractId: this.roundArguments.contractId,
+  //       contractFunction: "mint",
+  //       invokerIdentity: "client",
+  //       contractArguments: [assetId, tokenSymbol, "100", "user1"],
+  //       readOnly: false,
+  //     };
+
+  //     await this.sutAdapter.sendRequests(txArgs);
+  //   }
+  // }
 
   async submitTransaction() {
     const randomId = Math.floor(Math.random() * this.roundArguments.assets) + 1;
+    const assetId = `${this.workerIndex}-${randomId}`;
+    const tokenSymbol = `TTN${assetId}`;
 
     let txArgs = {
       contractId: this.roundArguments.contractId,
-      contractFunction: "updateAsset",
+      contractFunction: "mint",
       invokerIdentity: "client",
-      contractArguments: [`${this.workerIndex}-${randomId}`, '100', '100000'],
+      contractArguments: [assetId, tokenSymbol, "100", "user1"],
       readOnly: false,
     };
 
     return this.sutAdapter.sendRequests(txArgs);
   }
 
-  async cleanupWorkloadModule() {
-    for (let i = 1; i <= this.roundArguments.assets; i++) {
-      const assetId = `${this.workerIndex}-${i}`;
+  // async cleanupWorkloadModule() {
+  //   for (let i = 1; i <= this.roundArguments.assets; i++) {
+  //     const assetId = `${this.workerIndex}-${i}`;
 
-      console.log(
-        `Deleting asset "${assetId}" by workerNode ${this.workerIndex}`
-      );
+  //     console.log(
+  //       `Deleting asset "${assetId}" by workerNode ${this.workerIndex}`
+  //     );
 
-      let txArgs = {
-        contractId: this.roundArguments.contractId,
-        contractFunction: "deleteAsset",
-        invokerIdentity: "client",
-        contractArguments: [assetId],
-        readOnly: false,
-      };
+  //     let txArgs = {
+  //       contractId: this.roundArguments.contractId,
+  //       contractFunction: "deleteAsset",
+  //       invokerIdentity: "client",
+  //       contractArguments: [assetId],
+  //       readOnly: false,
+  //     };
 
-      await this.sutAdapter.sendRequests(txArgs);
-    }
-  }
+  //     await this.sutAdapter.sendRequests(txArgs);
+  //   }
+  // }
 }
 
 function createWorkloadModule() {
