ba64033f7dd6a2eb149287a473cf1b00f5c7db4d
jaewon1022 <andyjaewon@naver.com>
2024-11-11 15:59:20 +0900

    WRITE TRANSACTION 내 불필요한 initialize, cleanup 모듈 삭제 및 토큰 발행 기능만 수행하도록 수정

--- caliper-workspace/workload/write.js
+++ caliper-workspace/workload/write.js
@@ -7,78 +7,20 @@ class WriteWorkload extends WorkloadModuleBase {
     super();
   }
 
-  // async initializeWorkloadModule(
-  //   workerIndex,
-  //   totalWorkers,
-  //   roundIndex,
-  //   roundArguments,
-  //   sutAdapter,
-  //   sutContext
-  // ) {
-  //   await super.initializeWorkloadModule(
-  //     workerIndex,
-  //     totalWorkers,
-  //     roundIndex,
-  //     roundArguments,
-  //     sutAdapter,
-  //     sutContext
-  //   );
-
-  //   for (let i = 1; i <= this.roundArguments.assets; i++) {
-  //     const assetId = `${this.workerIndex}-${i}`;
-  //     const tokenSymbol = `TTN${assetId}`;
-
-  //     console.log(
-  //       `Creating asset "${this.workerIndex}-${i}" by workerNode ${workerIndex}`
-  //     );
-
-  //     let txArgs = {
-  //       contractId: this.roundArguments.contractId,
-  //       contractFunction: "mint",
-  //       invokerIdentity: "client",
-  //       contractArguments: [assetId, tokenSymbol, "100", "user1"],
-  //       readOnly: false,
-  //     };
-
-  //     await this.sutAdapter.sendRequests(txArgs);
-  //   }
-  // }
-
   async submitTransaction() {
-    const randomId = Math.floor(Math.random() * this.roundArguments.assets) + 1;
-    const assetId = `${this.workerIndex}-${randomId}`;
-    const tokenSymbol = `TTN${assetId}`;
+    const crypto = require("crypto");
+    const id = crypto.randomBytes(16).toString("hex");
 
-    let txArgs = {
+    const txArgs = {
       contractId: this.roundArguments.contractId,
       contractFunction: "mint",
       invokerIdentity: "client",
-      contractArguments: [assetId, tokenSymbol, "100"],
+      contractArguments: [id, "100"],
       readOnly: false,
     };
 
     return this.sutAdapter.sendRequests(txArgs);
   }
-
-  // async cleanupWorkloadModule() {
-  //   for (let i = 1; i <= this.roundArguments.assets; i++) {
-  //     const assetId = `${this.workerIndex}-${i}`;
-
-  //     console.log(
-  //       `Deleting asset "${assetId}" by workerNode ${this.workerIndex}`
-  //     );
-
-  //     let txArgs = {
-  //       contractId: this.roundArguments.contractId,
-  //       contractFunction: "deleteAsset",
-  //       invokerIdentity: "client",
-  //       contractArguments: [assetId],
-  //       readOnly: false,
-  //     };
-
-  //     await this.sutAdapter.sendRequests(txArgs);
-  //   }
-  // }
 }
 
 function createWorkloadModule() {
